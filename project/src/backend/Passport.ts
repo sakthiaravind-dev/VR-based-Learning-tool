4039aa8bf3d3d7be9d137ef5d6e34f4d17636b11
Baalekshan <baalekshan@gmail.com>
2025-02-27 16:52:43 +0530

    made deployment changes

--- project/src/backend/Passport.ts
+++ project/src/backend/Passport.ts
@@ -19,7 +19,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: 'http://localhost:5000/api/auth/google/callback',
+      callbackURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://vr-based-learning-tool.onrender.com/api/auth/google/callback'
+        : 'http://localhost:5000/api/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log('Google Profile:', profile);
